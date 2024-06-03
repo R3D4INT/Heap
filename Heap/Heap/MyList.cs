3b3a226a26361c6b87abbb43a350f0994f081acc
R3D4INT <khokhulya2006@gmail.com>
2024-06-03 23:04:49 +0200

    Impliment Heap

---
+++ Heap/Heap/MyList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class MyList
+    {
+        private int[] data;
+        private int maxSize;
+        private int currentSize;
+
+        public MyList(int size)
+        {
+            maxSize = size;
+            data = new int[maxSize];
+            currentSize = 0;
+        }
+
+        public bool isEmpty()
+        {
+            return currentSize == 0;
+        }
+
+        public bool isFull()
+        {
+            return currentSize == maxSize;
+        }
+
+        public void printList()
+        {
+            Console.WriteLine("List:");
+            for (var i = 0; i < currentSize; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void HeapSort(bool ascending)
+        {
+            for (var i = data.Length / 2 - 1; i >= 0; i--)
+            {
+                Heapify(data, data.Length, i, ascending);
+            }
+
+            for (var i = data.Length - 1; i >= 0; i--)
+            {
+                (data[0], data[i]) = (data[i], data[0]);
+
+                Heapify(data, i, 0, ascending);
+            }
+        }
+        public void BuildHeap(int[] data)
+        {
+            var n = data.Length;
+            for (var i = n / 2; i > 0; i--)
+            {
+                Heapify(data, n, i, true);
+            }
+
+            this.data = data;
+        }
+
+        private void Heapify(int[] arr, int n, int i, bool ascending)
+        {
+            var largest = i;
+            var l = 2 * i + 1;
+            var r = 2 * i + 2;
+            if (ascending)
+            {
+                if (l < n && arr[l] > arr[largest])
+                {
+                    largest = l;
+                }
+                if (r < n && arr[r] > arr[largest])
+                {
+                    largest = r;
+                }
+            }
+            else
+            {
+                if (l < n && arr[l] < arr[largest])
+                {
+                    largest = l;
+                }
+                if (r < n && arr[r] < arr[largest])
+                {
+                    largest = r;
+                }
+            }
+
+            if (largest != i)
+            {
+                (arr[i], arr[largest]) = (arr[largest], arr[i]);
+                Heapify(arr, n, largest, ascending);
+            }
+        }
+        public void Add(int item)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("List is full.");
+                return;
+            }
+            data[currentSize++] = item;
+        }
+        public int Size()
+        {
+            return currentSize;
+        }
+    }
+}
---
+++ Heap/Heap/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Heap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList(10);
+
+            list.Add(4);
+            list.Add(10);
+            list.Add(3);
+            list.Add(5);
+            list.Add(2);
+            list.Add(9);
+            list.Add(15);
+            list.Add(1);
+            list.Add(7);
+            list.Add(6);
+            list.Add(0);
+            list.Add(0);
+            list.Add(0);
+
+            list.BuildHeap(new int[] { 10, 5, 12, 3, 2, 1, 8, 7, 9, 4 });
+            list.printList();
+            Console.WriteLine("Unsorted:");
+            list.printList();
+            list.HeapSort(false);
+            Console.WriteLine("Sorted:");
+            list.printList();
+            Console.ReadKey();
+        }
+    }
+}
